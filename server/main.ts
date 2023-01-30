e98afb140ff957da1ba5dd51feefc0b0fea330cc
caspiankullgren <54362569+caspiankullgren@users.noreply.github.com>
2023-01-30 16:58:05 +0100

    Some added basic code

---
+++ server/main.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+
+const app : Express = express();
+
+/*app.get("/*.html",
+    async (req : Request, res : Response, next) => {
+      req.url = req.url.replace(".html", "");
+      res.redirect(req.url);
+});*/
+
+app.use(express.static("../client/public/"));
+app.use(require("./router/indexRouter"));
+app.use(require("./router/homepageRouter"));
+app.use(require("./router/postRouter"));
+app.use(require("./router/accountRouter"));
+
+
+app.get("/",
+    async (req : Request, res : Response) => {
+    res.redirect("index");
+});
+
+app.listen(8080);
--- server/service/post.service.ts
+++ server/service/post.service.ts
@@ -2,6 +2,15 @@ import { Post } from "../model/post.interface";
 import { Comment } from "../model/comment.interface";
 
 export interface IPostService {
+    // Returns the name of author
+    getAuthor() : Promise<string>;
+
+    // Returns the title of post
+    getTitle() : Promise<string>;
+    
+    // Returns the content of the post
+    getContent() : Promise<string>;
+
     // Returns a list of comments inside a forum post.
     getComments() : Promise<Array<Comment>>;
 
@@ -19,9 +28,45 @@ export interface IPostService {
 }
 
 class PostService implements IPostService {
+    post : Post = new Post("Author", "Title", "Content");
     comments : Array<Comment> = [];
+
+    async getAuthor(): Promise<string> {
+        return this.post.author
+    }
+
+    async getTitle(): Promise<string> {
+        return this.post.author;
+    }
+
+    async getContent(): Promise<string> {
+        return this.post.content;
+    }
 
     async getComments(): Promise<Comment[]> {
         return this.comments;
+    }
+
+    async submitComment(comment : Comment) : Promise<Boolean> {
+        this.comments.push(comment);
+        return true;
+    }
+
+    async upvoteComment(n : number) : Promise<Boolean> {
+        let c = this.comments[n];
+        if (c) {
+            c.upvote();
+            return true;
+        }
+        return false;
+    }
+
+    async downvoteComment(n : number) : Promise<Boolean> {
+        let c = this.comments[n];
+        if (c) {
+            c.downvote();
+            return true;
+        }
+        return false;
     }
 }
