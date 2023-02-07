f927326b6f1b34ad07dda614d36aa21168516daa
Adam Norberg <adam@dackie.dev>
2023-02-07 12:45:42 +0100

    Initialised / Setup Lab3
    Co-Authored-By: RavCarl <60098969+RavCarl@users.noreply.github.com>
    Co-Authored-By: johedvi <johedvi@student.chalmers.se>
    Co-Authored-By: Caspian <54362569+caspiankullgren@users.noreply.github.com>

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { forumRouter } from "./router/forumRouter";
 import { postRouter } from "./router/postRouter";
+import cors from "cors";
 export const app = express();
 
 /* Allows the use of JSON in response/request */
 app.use(express.json());
+app.use(cors());
 app.use("/forum",forumRouter); // Handle all /forum requests
 app.listen(3000);
