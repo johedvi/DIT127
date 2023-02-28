4a43ca39bed14c0ac072438ce92755c81fa9a372
caspiankullgren <54362569+caspiankullgren@users.noreply.github.com>
2023-02-28 13:35:20 +0100

    account css

--- client/src/AccountPage.tsx
+++ client/src/AccountPage.tsx
@@ -29,6 +29,7 @@ export default function (props: {}) {
 
   function signin() {
     return (
+      require("./account.css"),
       <div className="Auth-form-container">
         <form className="Auth-form" onSubmit={async e => {
           e.preventDefault();
