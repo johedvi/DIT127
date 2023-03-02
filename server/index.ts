dd760e494a391e6fa9493e1058d5843aa66e9e04
caspiankullgren <54362569+caspiankullgren@users.noreply.github.com>
2023-03-02 23:53:46 +0100

    F

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { forumRouter } from "./router/forumRouter";
+import { forumRouter } from "./router/ForumRouter";
 import { postRouter } from "./router/postRouter";
 import { userRouter } from "./router/userRouter";
 import session from "express-session";
