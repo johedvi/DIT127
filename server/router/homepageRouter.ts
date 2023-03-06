67bd79ccf0a443232d50cabce769a36ce43e05ff
caspiankullgren <54362569+caspiankullgren@users.noreply.github.com>
2023-03-06 14:57:50 +0100

    waste

--- server/router/homepageRouter.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from "express";
-const router = express.Router();
-
-router.get("/homepage",
-    async (req : Request, res : Response) => {
-        res.status(200).sendFile("homepage.html", {root : '../client/public/'});
-});
-
-module.exports = router;
