76706386c79a1a0807bccc4481b8cf08bdeac955
caspiankullgren <54362569+caspiankullgren@users.noreply.github.com>
2023-03-06 16:15:00 +0100

    refactor

--- server/builddocs.bat
+++ server/builddocs.bat
@@ -1,4 +1,11 @@
 @echo OFF
 (npx tsc
 @echo ON
-npx jsdoc ./router ./model ./service ./db -d %~dp0/DOCS)
+npx jsdoc ./router ./model ./service ./db -d %~dp0/DOCS
+@echo OFF
+cd %~dp0/router && del *.js*
+cd %~dp0/service && del *.js*
+cd %~dp0/model && del *.js*
+cd %~dp0/tests && del *.js*
+cd %~dp0
+)
